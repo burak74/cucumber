08c04cd663cccd72f36a0e7caefc3424df5f1d42
burak74 <burakalkan74@gmail.com>
2024-04-07 02:09:27 +0300

    TestDataBuild class and Utilities class added

--- APIFramework/src/test/java/stepDefinitions/StepDefinition.java
+++ APIFramework/src/test/java/stepDefinitions/StepDefinition.java
@@ -1,61 +1,35 @@
 package stepDefinitions;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.*;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import pojo.AddPlace;
-import pojo.Location;
-import static io.restassured.specification.ResponseSpecification.*;
+import resources.TestDataBuild;
+import resources.Utilities;
 
 
-public class StepDefinition {
+public class StepDefinition extends Utilities {
 	
 	ResponseSpecification resspec;
 	RequestSpecification res;
-	RequestSpecification req;
 	Response response;
 	JsonPath js;
+	TestDataBuild tdb = new TestDataBuild();
+	Utilities ut = new Utilities();
 	
 	@Given("Add Place Payload")
 	public void add_place_payload() {
-		RestAssured .baseURI="https://rahulshettyacademy.com";
-
-		AddPlace p =new AddPlace();
-		p.setAccuracy(50);
-		p.setAddress("29, side layout, cohen 09");
-		p.setLanguage("French-IN");
-		p.setPhone_number("(+91) 983 893 3937");
-		p.setWebsite("https://rahulshettyacademy.com");
-		p.setName("Frontline house");
-		List<String> myList =new ArrayList<String>();
-		myList.add("shoe park");
-		myList.add("shop");
-
-		p.setTypes(myList);
-		Location l =new Location();
-		l.setLat(-38.383494);
-		l.setLng(33.427362);
-		p.setLocation(l);
-
-		req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
-				.setContentType(ContentType.JSON).build();
-		 
-		 
+		
 		resspec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-		res = RestAssured.given().spec(req).body(p);
+		res = RestAssured.given().spec(ut.requestSpecification()).body(tdb.addPlacePayload());
 	}
 	
 	@When("user calls {string} with Post http request")
